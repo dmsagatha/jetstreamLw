36137889a3e360d42054a2b98572477cb430d2f3
Doris Morales <dmorales55@gmail.com>
2021-06-28 10:49:36 -0500

    Posts - Jack of Traits

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\Posts\Posts;
+use App\Http\Livewire\Admin\Post\Post;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 // Coders Free - https://codersfree.com/course-status/crea-aplicaciones-web-dinamicas-con-laravel-livewire
 Route::middleware(['auth:sanctum', 'verified'])->get('/posts', Posts::class)->name('posts');
+
+// Jack of Traits - https://www.youtube.com/watch?v=4muQp-nB0ZQ&list=PLSP81gW0XjNFP8RTBLMb1KL7Qbr8hUjr4&index=1
+// Route::middleware(['auth:sanctum', 'verified'])->get('/post', Post::class)->name('post');
