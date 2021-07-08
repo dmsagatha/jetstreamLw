23eed4145189066e8eb6142137d140960147093b
Doris Morales <dmorales55@gmail.com>
2021-07-08 16:27:33 -0500

    Categories: Listado

---
+++ app/Http/Livewire/Admin/Categories/Categories.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Categories;
+
+use Livewire\Component;
+
+class Categories extends Component
+{
+  public function render()
+  {
+    return view('admin.categories.index');
+  }
+}
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -21,6 +21,9 @@
           <x-jet-nav-link href="{{ route('user.list') }}" :active="request()->routeIs('user.list')">
             {{ __('Users') }}
           </x-jet-nav-link>
+          <x-jet-nav-link href="{{ route('categories') }}" :active="request()->routeIs('categories')">
+            Categorías
+          </x-jet-nav-link>
         </div>
       </div>
 
@@ -164,6 +167,9 @@
       </x-jet-responsive-nav-link>
       <x-jet-responsive-nav-link href="{{ route('user.list') }}" :active="request()->routeIs('user.list')">
         {{ __('Users') }}
+      </x-jet-responsive-nav-link>
+      <x-jet-responsive-nav-link href="{{ route('categories') }}" :active="request()->routeIs('categories')">
+        Categorías
       </x-jet-responsive-nav-link>
     </div>
 
