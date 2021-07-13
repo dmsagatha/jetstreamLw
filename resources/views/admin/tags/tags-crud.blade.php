42bdf850138985f4defc3bfacef11be3fac45266
Doris Morales <dmorales55@gmail.com>
2021-07-12 21:04:08 -0500

    Tags: Menus

---
+++ resources/views/admin/tags/tags-crud.blade.php
@@ -0,0 +1,3 @@
+<div>
+  <h1>Etiquetas</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Livewire\Admin\Posts\Posts;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Livewire\Admin\Categories\Categories;
+use App\Http\Livewire\Admin\Tags;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function ()
@@ -27,4 +28,8 @@ Route::group(['middleware' => 'auth:sanctum'], function ()
 
   // http://www.ascsoftwares.com/technology/laravel-livewire-crud-pagination-using-tailwind-modal/
   Route::get('/categorias', Categories::class)->name('categories');
+
+  // Sin Modal
+  // Laraveller - Laravel Livewire Tutorial #5 Create Read Delete Data on Database - https://www.youtube.com/watch?v=VlT48yRlIN4&list=PL6tf8fRbavl2LBvhQnxLBdNANEJTwA5an&index=5
+  Route::get('/etiquetas', Tags::class)->name('tags');
 });
