96ee5e294eff08b1f959a318a24ae2f4bdb1e454
Doris Morales <dmorales55@gmail.com>
2021-07-20 13:44:35 -0500

    Etiquetas flotantes con Tailwind CSS 2.2.4

--- resources/views/admin/tags/tags-crud.blade.php
+++ resources/views/admin/tags/tags-crud.blade.php
@@ -24,13 +24,7 @@
               <span class="mr-2">registros</span>
             </div><!-- select -->
 
-            <div class="relative flex-1">
-              <input wire:model="search" type="text" class="peer h-10 w-full border border-1.5 rounded-md border-gray-300 text-sm text-gray-900 placeholder-transparent focus:outline-none focus:border-light-blue-500 focus:border-2 p-3" placeholder="search..." />
-              <label for="search" class="absolute left-2 px-1 -top-2.5 bg-white text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-gray-600 peer-focus:text-sm">Escribir el término de busquedad</label>
-              <div class="absolute right-0 top-0 mt-2 mr-2">
-                <i class="fa fa-search h-6 w-6 text-gray-400"></i>
-              </div>
-            </div>
+            <x-search name="search" label="Escribir el término de busquedad" />
   
             @if ($search !== '')
               <button wire:click="clearPage" class="ml-2">
