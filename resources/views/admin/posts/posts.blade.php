258869bdc4d8718e9d2bcd211ae1d6ae5fb285a9
Doris Morales <dmorales55@gmail.com>
2021-08-01 23:30:18 -0500

    CRUD con Livewire y Tailwind Css

--- resources/views/admin/posts/posts.blade.php
+++ resources/views/admin/posts/posts.blade.php
@@ -1,4 +1,4 @@
-<div wire:init="loadRecords" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+<div wire:init="loadRecords" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
   @section('title', 'Posts')
 
   <x-slot name="header">
