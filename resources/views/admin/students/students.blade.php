82d2185230783c33670b6023dc982c3dfc7aa115
Doris Morales <dmorales55@gmail.com>
2021-08-01 22:22:19 -0500

    Componente Search

--- resources/views/admin/students/students.blade.php
+++ resources/views/admin/students/students.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
   @section('title', 'Estudiantes')
 
   <x-slot name="header">
@@ -10,9 +10,8 @@
   <div class="py-2">@include('shared._messages')</div>
 
   <x-table>
-    <div class="items-center justify-between bg-white px-4 py-3 border-t border-gray-200 sm:px-6">
-      <!-- Paginador y Buscador -->
-      <div class="flex items-center text-gray-500 text-sm">
+    <div class="flex items-center justify-center text-sm bg-white px-4 py-3 gap-x-2 border-t border-gray-200 sm:px-6">
+      <div class="flex flex-wrap items-center mt-3">
         <label for="perPage">Mostrar</label>
         <select wire:model="perPage" class="mx-2 form-control">
           <option value="5">5</option>
@@ -23,54 +22,59 @@
         </select>
 
         <span>registros</span>
+      </div>
 
+      <div class="relative flex-1 mx-4">
         <x-search name="search" label="Término de búsqueda" />
+        <div class="absolute right-0 top-0 mt-2 mr-2">
+          @if ($search !== '')
+            <button wire:click="clearPage">
+              <i class="fa fa-eraser"></i>
+            </button>
+          @else
+            <i class="fa fa-search h-6 w-6 text-gray-400"></i>
+          @endif
+        </div>
+      </div>
 
-        @if ($search !== '')
-          <button wire:click="clearPage" class="ml-2">
-            <i class="fa fa-eraser"></i>
-          </button>
-        @endif
+      <div class="self-center">
+        <label for="perPage" class="py-3">Filtrar Lecciones</label>
+        <select class="mx-2 form-control" wire:model="selectedLesson">
+          <option value="">Seleccionar</option>
+          @foreach ($lessons as $item)
+            <option value="{{ $item->id }}">{{ $item->name }}</option>
+          @endforeach
+        </select>
+      </div>
 
-        <div class="px-2 py-4 flex items-center">
-          <label for="perPage">Filtrar por Clases</label>
-          <select class="mx-2 form-control" wire:model="selectedLesson">
-            <option value="">Todas las Clases</option>
-            @foreach ($lessons as $item)
+      @if ($selectedLesson != 0 && !is_null($selectedLesson))
+        <div class="self-center">
+          <label for="perPage" class="py-3">Filtrar Secciones</label>
+          <select class="mx-2 form-control" wire:model="selectedSection">
+            <option value="">Seleccionar</option>
+            @foreach ($sections as $item)
               <option value="{{ $item->id }}">{{ $item->name }}</option>
             @endforeach
           </select>
         </div>
+      @endif
 
-        @if ($selectedLesson != 0 && !is_null($selectedLesson))
-          <div class="px-2 py-4 flex items-center">
-            <label for="perPage">Secciones</label>
-            <select class="mx-2 form-control" wire:model="selectedSection">
-              <option value="">Seleccionar Sección</option>
-              @foreach ($sections as $item)
-                <option value="{{ $item->id }}">{{ $item->name }}</option>
-              @endforeach
-            </select>
+      @if ($checked)
+        <div x-data="{ open: false }" class="relative">
+          <!-- Dropdown toggle button -->
+          <button @click="open = true" class="flex w-44 items-center p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+            <span class="mr-4">Eliminar seleccionados ({{ count($checked) }})</span>
+            <i class="fas fa-angle-down"></i>
+          </button>
+          <!-- Dropdown List -->            
+          <div x-show="open" @click.away="open = false" class="absolute w-44 right-0 py-2 mt-1 bg-white divide-y divide-gray-600 rounded-md shadow-xl">
+            <a href="#" type="button" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-400 hover:text-white" onclick="confirm('Esta seguro de eliminar los registros seleccionados?') || event.stopImmediatePropagation()" wire:click="deleteRecords()">
+              <i class="far fa-trash-alt text-red-600 mr-2"></i>Eliminar
+            </a>
           </div>
-        @endif
-
-        @if ($checked)
-          <div x-data="{ open: false }" class="relative">
-            <!-- Dropdown toggle button -->
-            <button @click="open = true" class="flex w-44 items-center p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              <span class="mr-4">Eliminar seleccionados ({{ count($checked) }})</span>
-              <i class="fas fa-angle-down"></i>
-            </button>
-            <!-- Dropdown List -->            
-            <div x-show="open" @click.away="open = false" class="absolute w-44 right-0 py-2 mt-2 bg-white divide-y divide-gray-600 rounded-md shadow-xl">
-              <a href="#" type="button" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-400 hover:text-white" onclick="confirm('Esta seguro de eliminar los registros seleccionados?') || event.stopImmediatePropagation()" wire:click="deleteRecords()">
-                <i class="far fa-trash-alt text-red-600 mr-2"></i>Eliminar
-              </a>
-            </div>
-          </div>
-        @endif
-      </div><!-- Paginador y Buscador -->
-    </div><!-- items-center justify-between -->
+        </div>
+      @endif
+    </div><!-- Paginador, Buscador y Filtros -->
 
     {{-- @foreach ($checked as $item)
       {{ $item }}
