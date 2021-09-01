862eed68de34de6cd6892234c814f53d1d141624
Doris Morales <dmorales55@gmail.com>
2021-09-01 18:43:21 -0500

    Navbar y Sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,16 @@
 
       {{-- Navbar - Barra de navegación --}}
       <div class="relative z-0 lg:flex-grow">
+        {{-- <header class="flex bg-gray-700 text-white items-center">
+          <!-- Botón Sidebar -->
+          <button @click="open = true"
+            class="p-2 focus:outline-none focus:bg-gray-600 hover:bg-gray-600 rounded-md lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+            </svg>
+          </button>
+          <span class="block text-white font-semibold text-2xl sm:text-3xl p-4">Jetstream</span>
+        </header> --}}
         @include('layouts.includes.navbar')
       
         <!-- Page Heading -->
