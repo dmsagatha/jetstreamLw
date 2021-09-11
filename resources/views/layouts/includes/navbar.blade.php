b2e449a8430f28c56eaeff998fbc532db6995c23
Doris Morales <dmorales55@gmail.com>
2021-09-11 10:46:10 -0500

    Navbar y Sidebar con TailwindCss y AlpineJs

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -43,16 +43,34 @@
           <div class="sm:flex flex-1 items-center px-2 pt-2 pb-3 space-y-1">
           {{-- <div class="list-reset md:flex flex-1 items-center px-4 md:px-0"> --}}
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-            aria-current="page">Dashboard</a>
-            <a href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-            <a href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-            <a href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
-            <a href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+            <a href="{{ route('dashboard') }}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+            aria-current="page">
+              Dashboard
+            </a>
+            <a href="{{ route('tags') }}"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Etiquetas
+            </a>
+            <a href="{{ route('students') }}"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Estudiantes
+            </a>
+            <a href="{{ route('products') }}"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Productos
+            </a>
+            <a href="{{ route('posts') }}"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Posts
+            </a>
+            <a href="{{ route('user.list') }}"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Usuarios
+            </a>
+            <a href="{{ route('categories') }}"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Categorías
+            </a>
           </div>
         </div>
         <!-- Menu izquierdo -->
