bd6846609f4fdf06dc0abdb3d35aa425cc662082
Doris Morales <dmorales55@gmail.com>
2021-09-11 12:36:26 -0500

    Navbar y Sidebar con TailwindCss y AlpineJs

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -7,7 +7,7 @@
           class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
           aria-controls="mobile-menu" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
-          <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+          <svg fill="currentColor" viewBox="0 00 20" class="w-6 h-6">
             <path x-show="!openNavbar" fill-rule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
               clip-rule="evenodd"></path>
@@ -28,48 +28,69 @@
         </svg>
       </div>
 
-      <!-- Logo y Menu izquierdo -->
-      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-        <!-- Logo -->
-        <div class="flex-shrink-0 flex items-center">
-          <img class="flex lg:hidden h-8 w-auto ml-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-            alt="Workflow">
-          <img class="hidden lg:block h-8 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+      <!-- Logo -->
+      <div class="flex-shrink-0 flex items-center">
+        {{-- <img class="flex lg:hidden h-8 w-auto ml-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+          alt="Workflow">
+        <img class="hidden lg:block h-8 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"> --}}
+        <a href="{{ route('dashboard') }}" class="text-indigo-50 py-10 sm:py-4 text-xl hover:text-indigo-200">
+          <i class="fas fa-tachometer-alt fa-lg"></i></a>
+      </div>
+
+      <!-- Menú desplegable -->
+      <div class="flex items-center px-2 py-4 bg-gray-800 text-gray-300 hover:text-white cursor-pointer group relative w-40">
+        <span class="capitalize ml-1"><i class="fas fa-ellipsis-v mr-1"></i>Todos</span>
+
+        <div
+          class="absolute w-full left-0 top-full bg-white shadow-md pt-1 invisible opacity-0 group-hover:opacity-100 group-hover:visible transition duration-300 z-50 divide-y divide-gray-300 divide-dashed overflow-hidden">
+          <a href="{{ route('tags') }}" class="flex items-center px-4 py-3 hover:bg-gray-100 text-gray-600 hover:text-semibold transition">
+            <i class="fas fa-tags text-gray-600 text-sm mr-1"></i>Etiquetas
+          </a>
+          <a href="{{ route('students') }}" class="flex items-center px-4 py-3 hover:bg-gray-100 text-gray-600 hover:text-semibold transition">
+            <i class="fas fa-users text-gray-600 text-sm mr-1"></i>Estudiantes
+          </a>
+          <a href="{{ route('products') }}" class="flex items-center px-4 py-3 hover:bg-gray-100 text-gray-600 hover:text-semibold transition">
+            <i class="fab fa-product-hunt text-gray-600 text-sm mr-1"></i>Productos
+          </a>
+          <a href="{{ route('posts') }}" class="flex items-center px-4 py-3 hover:bg-gray-100 text-gray-600 hover:text-semibold transition">
+            <i class="fas fa-tags text-gray-600 text-sm mr-1"></i>Publicaciones
+          </a>
+          <a href="{{ route('user.list') }}" class="flex items-center px-4 py-3 hover:bg-gray-100 text-gray-600 hover:text-semibold transition">
+            <i class="fas fa-tags text-gray-600 text-sm mr-1"></i>Usuaros
+          </a>
+          <a href="{{ route('categories') }}" class="flex items-center px-4 py-3 hover:bg-gray-100 text-gray-600 hover:text-semibold transition">
+            <i class="fas fa-tags text-gray-600 text-sm mr-1"></i>Categorías
+          </a>
         </div>
-        
+      </div>
+      <!-- Menú desplegable -->
+
+      <!-- Menu izquierdo -->
+      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">        
         <!-- Menu izquierdo -->
         <div :class="openNavbar ? 'show' : 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto sm:mt-0 bg-gray-800 z-20 absolute sm:relative sm:top-0 top-16 left-0">
           <div class="sm:flex flex-1 items-center px-2 pt-2 pb-3 space-y-1">
-          {{-- <div class="list-reset md:flex flex-1 items-center px-4 md:px-0"> --}}
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="{{ route('dashboard') }}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+            {{-- <a href="{{ route('dashboard') }}" class="bg-gray-900 text-white block px-2 py-2 rounded-md text-base font-medium"
             aria-current="page">
               Dashboard
-            </a>
+            </a> --}}
             <a href="{{ route('tags') }}"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-2 py-2 rounded-md text-base font-medium">
               Etiquetas
             </a>
-            <a href="{{ route('students') }}"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Estudiantes
-            </a>
             <a href="{{ route('products') }}"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-2 py-2 rounded-md text-base font-medium">
               Productos
             </a>
             <a href="{{ route('posts') }}"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-2 py-2 rounded-md text-base font-medium">
               Posts
             </a>
             <a href="{{ route('user.list') }}"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-2 py-2 rounded-md text-base font-medium">
               Usuarios
-            </a>
-            <a href="{{ route('categories') }}"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Categorías
             </a>
           </div>
         </div>
@@ -90,39 +111,49 @@
           </svg>
         </button>
 
-        <!-- Profile dropdown -->
-        <div x-data="{ openDropdown: false }" class="ml-3 relative">
-          <div>
-            <button x-on:click="openDropdown = true" type="button"
-              class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="sr-only">Open user menu</span>
-              <img class="h-8 w-8 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt="">
-            </button>
-          </div>
-          <div x-show="openDropdown" x-on:click.away="openDropdown = false"
-            class="origin-top-right absolute right-0 mt-4 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <!-- Active: "bg-gray-100", Not Active: "" -->
-            <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700">
-              
-            </a>
-            <hr class="dropdown-divider" /></li>
-            <a href="#!" class="block px-4 py-2 text-sm text-gray-700">
-              Configuraciones
-            </a>
+        <!-- Menú Desplegable -->
+        <div x-data="{ dropdownOpen: false}" @click.away="dropdownOpen = false" class="relative">
+          <button @click="dropdownOpen = !dropdownOpen" class="flex w-full text-white px-2 py-2 mt-2 md:mt-0">{{ Auth::user()->name }}
+            <img src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" class="w-6 h-6 rounded-full ml-2">
+            <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': dropdownOpen, 'rotate-0': !dropdownOpen}"
+              class="inline w-6 h-6 mt-1 ml-2 transition-transform duration-200 transform md:-mt-1">
+              <path fill-rule="evenodd"
+                d="M5.292 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </button>
 
-            <form method="POST" action="{{ route('logout') }}">
-              @csrf
-
-              <a href="{{ route('logout') }}"onclick="event.preventDefault(); this.closest('form').submit();" class="block px-4 py-2 text-sm text-gray-700">
-                <i class="fa fa-sign-out-alt text-red-400 mr-2"></i>{{ __('Log Out') }}
+          <!-- origin-top-right right-1 ring-black ring-opacity-5 divide-y divide-gray-100 md:w-44 md:mt-0 -->
+          <ul x-show="dropdownOpen"
+            class="md:absolute bg-white right-0 rounded-md mt-2 overflow-hidden shadow-xl z-10 md:w-40 space-y-2 "
+            x-transition:enter="transition-transform transition-opacity ease-out duration-300"
+            x-transition:enter-start="opacity-0 transform -translate-y-2"
+            x-transition:enter-end="opacity-100 transform translate-y-0"
+            x-transition:leave="transition ease-in duration-300"
+            x-transition:leave-end="opacity-0 transform -translate-y-3"
+            x-transition:leave-start="transform opacity-100 scale-100">
+            <li>
+              <a href="{{ route('profile.show') }}" class="flex px-4 py-2 text-sm font-medium text-gray-700 hover:bg-lime-50">
+                Perfil
               </a>
-            </form>
-          </div>
+            </li>
+            <li>
+              <a href="#!" class="flex px-4 py-2 text-sm font-medium text-gray-700 hover:bg-lime-50">
+                Publicaciones
+              </a>
+            </li>
+            <li>
+              <form method="POST" action="{{ route('logout') }}">
+                @csrf
+  
+                <a href="{{ route('logout') }}"onclick="event.preventDefault(); this.closest('form').submit();" class="flex px-4 py-2 text-sm font-medium text-gray-700 hover:bg-lime-50">
+                  <i class="fa fa-sign-out-alt text-red-400 mr-2"></i>{{ __('Log Out') }}
+                </a>
+              </form>
+            </li>
+          </ul>
         </div>
+        <!-- Menú desplegable -->
       </div>
     </div>
   </div>
