6077d367ce9c818c5adbbcb56bf943dcd9e06b16
Doris Morales <dmorales55@gmail.com>
2021-09-26 16:29:23 -0500

    Appointments: CRUD

--- resources/views/admin/appointments/createUpdate.blade.php
+++ resources/views/admin/appointments/createUpdate.blade.php
@@ -36,7 +36,7 @@
 
             <div class="px-2 py-3 bg-gray-50 text-center sm:px-3">
               <x-button class="ml-3 mt-4">
-                <svg wire:loading wire:target="submitForm" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                <svg wire:loading wire:target="save" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
                   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                   <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                   <path class="opacity-75" fill="currentColor"
