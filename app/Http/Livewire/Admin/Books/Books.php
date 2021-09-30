7087d550e9de05b2e89c5254c6ae97609f3f5ba1
Doris Morales <dmorales55@gmail.com>
2021-09-30 17:22:29 -0500

    Books: Migracion, Seeders y Factories

---
+++ app/Http/Livewire/Admin/Books/Books.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Books;
+
+use Livewire\Component;
+
+class Books extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.books.books');
+    }
+}
