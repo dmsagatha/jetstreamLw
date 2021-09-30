afebf0e93d5e557d75302df9a1d4ed7615db5cee
Doris Morales <dmorales55@gmail.com>
2021-09-30 13:17:21 -0500

    Appointment: CRUD

--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -28,10 +28,6 @@
       <!-- Buscar -->
       <div class="relative flex-1 mx-4">
         <x-search name="search" label="Buscar término" />
-        <div class="la-ball-clip-rotate la-dark la-sm">
-          <div></div>
-        </div>
-
         <div class="absolute right-0 top-0 mt-2 mr-2">
           @if ($search !== '')
             <button wire:click="clearPage">
@@ -42,9 +38,6 @@
           @endif
         </div>
       </div>
-      {{-- <div class="relative flex-1 mx-4">
-        <input wire:model="searchTerm" type="text" class="peer h-10 w-full border border-1.5 rounded-md border-gray-300 text-sm text-gray-900 placeholder-transparent focus:outline-none focus:border-light-blue-500 focus:border-2 p-3" placeholder="Buscar...">
-      </div> --}}
       
       <!-- Botón Crear -->
       <div class="flex items-center">
